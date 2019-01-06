724f379beb3c6fe894665b786f0f0a94f28efaf5
wyan <121757925@qq.com>
2019-01-06 12:08:10 +0800

    提交并推送的测试

--- src/main/java/com/itheima/controller/BaseController.java
+++ src/main/java/com/itheima/controller/BaseController.java
@@ -6,4 +6,7 @@ package com.itheima.controller;
  * @Description:
  */
 public class BaseController {
+    public static void main(String[] args) {
+        System.out.println("tttttttttt");
+    }
 }
